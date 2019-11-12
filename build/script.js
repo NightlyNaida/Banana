d477c6cf765a77070396ae96fe1b8f9b54402d2f
NightlyNaida <sergio.in.odi@mail.ru>
2019-11-12 10:16:10 +0300

    AFosodf
    s;ldgfdsaf

--- build/script.js
+++ build/script.js
@@ -9,23 +9,70 @@ $('.button-menu').click(function(){
 
 function alo(){
 
-    var object = document.getElementById('banana-contur-box');
-    var contentDocument = object.contentDocument;
+  var objects = document.getElementsByClassName('banana-contur-box'); //храним переменные объектов с свг
+  var images = [];
+  for(let element of objects){
+    var contentDocument = element.contentDocument;
     var svgObject = contentDocument.getElementById('banana-contur');
-    //$(image).css('transition','x .6s ease-out, y .6s ease-out');
-
-    var body = document.body,
-        startX = -30,
-        startY = -30,
-        w = document.documentElement.offsetWidth,
-        h = document.documentElement.offsetHeight;
+    let image = svgObject.getElementById('image');
+    $(image).css('transition','x .6s ease-out, y .6s ease-out');
+    images.push(image);
+  }  
   
-      body.addEventListener('mousemove', function(evt){
-      var posX = Math.round(evt.clientX / w * startX)
-      var posY = Math.round(evt.clientY / h * startY)
+  var body = document.body,
+  startX = -60,
+  startY = -60,
+  w = document.documentElement.offsetWidth,
+  h = document.documentElement.offsetHeight;
+
+  body.addEventListener('mousemove', function(evt){
+    var posX = Math.round(evt.clientX / w * startX)
+    var posY = Math.round(evt.clientY / h * startY)
+    for(let image of images){
       $(image).css('x', posX+'px')
       $(image).css('y', posY+'px')
-    })
-  }
+      }
+  })
+}
 
-//setTimeout(alo, 1000);
+setTimeout(alo, 1000); 
+
+
+$(function(){
+  var neededSlide = $('.columns-box');
+  var block = true;
+
+  $(window).scroll(function(){
+    var scrollEvent = ($(window).scrollTop() > (neededSlide.position().top - $(window).height()));
+    
+    if(scrollEvent && block){
+
+      block = false;
+      
+      $('.banana-count').each(function () {
+        var countOfBananas = $(this).text();
+        console.log("countOfBananas is " + countOfBananas);
+        var relation = (countOfBananas / 25000) * 100;
+        console.log("relation is " + relation);
+        var parent = $(this).parents('.column-box');
+        console.log("Parent of " + $(this) + " is " + parent);
+
+        parent.each(function(){
+          $(this).css('height', Math.ceil(relation) + "%");
+          console.log("new height of " + $(this) + " is " + Math.ceil(relation) + "%")
+        })
+        
+        $(this).prop('Counter',0).animate({ 
+          Counter: $(this).text()
+        }, 
+        {
+          duration: 3000,
+      
+          step: function(now) {
+            $(this).text(Math.ceil(now));
+            }
+          });
+        })
+    }
+  })
+})
