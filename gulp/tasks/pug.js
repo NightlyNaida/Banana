bb47562720ce5283c245a7b3413e7c2027c06495
NightlyNaida <sergio.in.odi@mail.ru>
2020-02-15 11:19:40 +0300

    Создание страницы бананов

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -1,6 +1,6 @@
 module.exports = function(){
     $.gulp.task('pug',function () {
-        return $.gulp.src('src/frontend/pug/index.pug')
+        return $.gulp.src('src/frontend/pug/*.pug')
         .pipe($.pug({
             pretty:true
         }))
--- src/frontend/js/script.js
+++ src/frontend/js/script.js
@@ -1,5 +1,102 @@
+
+
+
+/*Поведение карточки банана*/
+$('.banana-item__rate-container').mouseleave(function(e){
+  let childArr = $(this).children('.banana-item__rate-item');// сгребаем все бананы в один массив
+  childArr.each(function(i,elem){
+      $(elem).css('transform','scale(1)');
+      paintBanana(elem,'none');
+  })
+})
+
+
+$('.banana-item__rate-item').mouseenter(function(e){
+  let selfParent = $(this).parent(); // получаем родительский блок
+  let index = $(this).index(); // получаем индекс текущего банана  под мышью
+  let childArr = $(selfParent).children('.banana-item__rate-item');// сгребаем все бананы в один массив
+  
+  childArr.each(function(i,elem){ 
+    if (i <= index){
+      $(elem).css('transform','scale(1.2)');
+      paintBanana(elem,'#FFE15A');
+    }
+    else if (i > index){
+      $(elem).css('transform','scale(1)');
+      paintBanana(elem, 'none');
+    }
+  })
+
+})
+
+
+$('.banana-item__rate-item').click(function(e){
+  paintBanana(e.target,'#ffffff');
+});
+
+function paintBanana(elem, color){
+  var object = $(elem).children('object')[0];
+  var content = object.contentDocument;
+  var svg = content.getElementById('banana-contur');
+  var svgPath = svg.getElementById('banana-contur-path');
+  svgPath.style.fill = color;
+}
+
+/*Конец поведения карточки банана*/
+
+
+
+
+var choiceBoxIsAdded = false; //флаг добавления всплывающего списка выбора метода сортировки
+
+$('.options-bar__sort-method-caption-container').mouseleave(function(){
+  if (choiceBoxIsAdded){
+    setTimeout(function(){$('.options-bar-sort-icon').toggleClass('options-bar-sort-icon-rotate');},20);//поворот стрелки в исходное положение  
+    // setTimeout - фикс, ибо скрипт срабатывает ьыстрее подгрузкки css кода
+  }
+  
+  let container = document.getElementsByClassName('options-bar__sort-method-caption-container')[0];//выдергиваем блок с заголовком
+  $('.chocieBox').remove();//удаляем лобавленный при клике список
+  $(container).css("border-radius", "10px");//закругляем все углы у блока заголовка
+  choiceBoxIsAdded = false;// выключаем флаг добавления списка метода сортировки
+})
+
+
+$('.options-bar__sort-method-caption-container').click(function(){
+  var code = "";// сюда складываем код списка
+  let container = document.getElementsByClassName('options-bar__sort-method-caption-container')[0];//выдергиваем блок загловка метода сортировки
+
+    let boxOpen = "<div class='chocieBox'>"; //Код списка разбитый по частям
+    let boxClose = "</div>";
+    let item = "<p class='value'></p>";
+    let values = ['по алфавиту', 'по популярности' , 'по рейтингу']; // сюда складываем необходимые методы сортировки
+
+    if (! choiceBoxIsAdded){ //Проверям, не добавлен ли список
+      setTimeout(function(){$('.options-bar-sort-icon').toggleClass('options-bar-sort-icon-rotate');},20);//Переворачиваем стрелку
+      for (i=0; i < values.length; i++) 
+      {
+          if (i == 0){ code += boxOpen } // во время первой итерации открываем блок списка 
+          code += item; // добавляем элементы списка
+          if (i == values.length - 1){ code += boxClose; choiceBoxIsAdded = true; } //закрываем блок списка во время последней итерации
+      }
+
+      container.innerHTML += code;// вставляем код списка в блок заголовка
+
+      $('.value').each(function(i,elem){ // вставляем в абзацы элементов списка значения из массива
+        elem.innerHTML += values[i];
+      })
+
+      var choiceBox = document.getElementsByClassName("chocieBox")[0];// выдергиваем список
+      $(container).css("border-radius", "10px 10px 0 0"); //выклюаем нижнее скругление у блока заголовка
+      setTimeout(function(){$('.chocieBox').toggleClass('chocieBox-show');},20);//показываем список (по умолчанию он скрыт и средствами css реализуется анимация появления) 
+    }
+})
+
+
 $('.button-search').click(function(){
-    $('.search-form__input').toggleClass('search-form__input-fw');
+    $('.search-form').toggleClass('search-form-show');// Управляем Пположением ФОРМЫ
+    $('.search-form__input').toggleClass('search-form__input-show');// Управляем внешним видом инпута
+    $('.button-search').toggleClass('button-search-pushed');// Управляем внешним видом кнопки на которую нажали
 })
 
 $('.button-menu').click(function(){
